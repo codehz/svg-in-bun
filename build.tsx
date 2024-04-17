1388edfef163f6c5708094f3f86080028fb18b8e
codehz <git@hertz.moe>
2024-04-17 15:25:35 +0800

    hidden by default

--- build.tsx
+++ build.tsx
@@ -4,7 +4,7 @@ import { get_defnitions, reset_database } from "./utils/db";
 export function getGeneratedSvg() {
   const defs = get_defnitions.values().flat().join("");
   return render(
-    <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="0" height="0">
       <defs dangerouslySetInnerHTML={{ __html: defs }} />
     </svg>
   );
